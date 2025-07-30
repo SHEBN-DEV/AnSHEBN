534154169475604b895436c0172c3fb8ebc152dc
NubiaACR <nubiandrea2001@gmail.com>
2025-07-30 12:22:39 -0500

    My Profile
    Cambio de contenido a mostrar

--- src/app/MyProfile/page.jsx
+++ src/app/MyProfile/page.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import Sidebar from "../components/Sidebar";
 
 const content = [
-    {experience: 'Software Developer, XYZ Tech Solutions Company in January 2020 - April 2023',
+    {
+    name: 'Maria Torre',
+    userName: 'mariatorre5',
+    experience: 'Software Developer, XYZ Tech Solutions Company in January 2020 - April 2023',
     about: 'I am a software developer passionate about creating innovative and efficient solutions, with a solid background in Systems Engineering and experience in technology such as JavaScript and Python. I specialize in the design and development of web and mobile applications, prioritizing functionality, user experience, and scalability.',
     location: 'Medellin, Colombia',
     portfolio: 'Upload your portfolio'
@@ -27,8 +30,8 @@ const MyProfile = () => {
                             <div className="w-3 h-3 md:w-5 md:h-5 bg-green-500 rounded-full md:absolute bottom-162 left-8 md:bottom-0 md:left-4"></div>
                         </div>
                         <div>
-                            <p className="text-3xl font-bold">Maria Torre</p>
-                            <p className="text-sm">mariatorre5</p>
+                            <p className="text-3xl font-bold">{content[0].name}</p>
+                            <p className="text-sm">{content[0].userName}</p>
                         </div>
                     </div>
                     <div className="w-1/2 md:relative flex justify-center pt-4">
@@ -41,14 +44,14 @@ const MyProfile = () => {
                 <div className="flex flex-col gap-10 p-4 md:py-10">
                     <div className="">
                         <p className="text-lg font-semibold">Experience</p>
-                        <p className="text-base">{content.experience}</p>
+                        <p className="text-base">{content[0].experience}</p>
                     </div>
                     <div className="w-5/6 border-b-2 border-gray-500 mx-auto"></div>
                     <div className="flex items-start justify-between">
                         <div className="w-1/2 flex flex-col gap-2">
                             <p className="text-lg font-semibold">About Me</p>
                             <p className="text-base">
-                                
+                                {content[0].about}
                             </p>
                             <a href="#" className="text-lg font-bold text-[#ff29d7] pt-6 hover:underline">Read More</a>
                         </div>
@@ -56,10 +59,10 @@ const MyProfile = () => {
                             <p className="text-lg font-semibold">Location</p>
                             <div className="flex items-center gap-x-2">
                                 <img className="w-10 h-10 rounded-full" src="/images/profile/fondo-inferior.png" alt="Pais" />
-                                <p></p>
+                                <p>{content[0].location}</p>
                             </div>
                             <p className="text-lg font-semibold pt-4">Portfolio</p>
-                            <a href="" className="text-base hover:text-[#ff29d7]"></a>
+                            <a href="" className="text-base hover:text-[#ff29d7]">{content[0].portfolio}</a>
                         </div>
                     </div>
                 </div>
